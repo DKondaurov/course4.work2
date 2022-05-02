7ce31acc7704c5c0378372f6651600d4f5e8855b
homework <9874@bk.ru>
2022-05-03 01:50:42 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course4/work2/model/StudentCount.java
@@ -0,0 +1,6 @@
+package pro.sky.java.course4.work2.model;
+
+public interface StudentCount {
+
+    String getStudent();
+}
---
+++ src/test/java/pro/sky/java/course4/work2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course4.work2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
